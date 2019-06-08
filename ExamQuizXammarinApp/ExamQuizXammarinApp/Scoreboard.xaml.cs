56bc0a551f8bc2b0eb3426eff8449cb125d757b7
MikoTheAble <fuksior99775533@gmail.com>
2019-06-08 18:55:44 +0200

    Adding new, empty subpages

---
+++ ExamQuizXammarinApp/ExamQuizXammarinApp/Scoreboard.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExamQuizXammarinApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Scoreboard : ContentPage
+	{
+		public Scoreboard ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
