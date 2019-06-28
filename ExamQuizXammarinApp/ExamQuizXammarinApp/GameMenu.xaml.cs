dd65bd835a6bf6d88bd8937c38737df377e98244
Kozłowski Filip <filip.kozlowski@microsoft.wsei.edu.pl>
2019-06-28 11:22:27 +0200

    Added colors to app

--- ExamQuizXammarinApp/ExamQuizXammarinApp/GameMenu.xaml.cs
+++ ExamQuizXammarinApp/ExamQuizXammarinApp/GameMenu.xaml.cs
@@ -6,19 +6,27 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using ExamQuizXammarinApp.Database;
 
 namespace ExamQuizXammarinApp
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class GameMenu : ContentPage
     {
+        
+        
+        
+
         public GameMenu()
         {
             InitializeComponent();
+            
+
         }
         private async void Button_Clicked_SuggestQuestion(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new NewQuestion());
+
         }
         
 
@@ -30,6 +38,8 @@ namespace ExamQuizXammarinApp
         private async void Button_Clicked_Scoreboard(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new Scoreboard());
+            
+
         }
     }
 }
--- ExamQuizXammarinApp/ExamQuizXammarinApp/Login.xaml.cs
+++ ExamQuizXammarinApp/ExamQuizXammarinApp/Login.xaml.cs
@@ -7,17 +7,22 @@ using ExamQuizXammarinApp.Database;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using ExamQuizXammarinApp.Database;
 
 namespace ExamQuizXammarinApp
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class Login : ContentPage
 	{
+        FirebaseHelper firebasehelper = new FirebaseHelper();
         public static int CurrentUserID = 0;
+        public  string login;
 		public Login ()
 		{
 			InitializeComponent ();
 		}
+       
+
 
         private async void Button_Clicked_CreateAccount(object sender, EventArgs e)
         {
@@ -44,7 +49,9 @@ namespace ExamQuizXammarinApp
                 var result = await firebaseHelper.FindUserByLoginAndPassword(entryLogin.Text, entryPassword.Text);
                 if (result != null)
                 {
-                    CurrentUserID = result.ID; 
+                    CurrentUserID = result.ID;
+                    var reault = await firebasehelper.GetUser(CurrentUserID);
+                    login = reault.Username;
                     await Navigation.PushAsync(new GameMenu());
                 }
                 else
@@ -54,6 +61,7 @@ namespace ExamQuizXammarinApp
                     await Navigation.PushAsync(new Login());
                 }
             }
+            
 
         }
     }
